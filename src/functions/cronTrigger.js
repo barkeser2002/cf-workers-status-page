2781db18d4b7921957737efd0199f9e2aecbf995
Barış Keser <45911502+barkeser2002@users.noreply.github.com>
2024-04-24 14:00:19 +0000

    ss17

--- src/functions/cronTrigger.js
+++ src/functions/cronTrigger.js
@@ -1,15 +1,3 @@
-module.exports = {
-  webpack: (config, options) => {
-    config.module.rules.push({
-      test: /\.ya?ml$/,
-      type: 'json',
-      use: 'yaml-loader',
-    })
-
-    return config
-  },
-}
-
 import config from '../../config.yaml'
 
 import {
